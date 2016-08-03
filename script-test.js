04a6865d1ad76147c93ab8810029ce1090674812
Nathan Tejuco <ntejuco@gmail.com>
2016-08-03 16:36:06 +1200

    initial commit

---
+++ script-test.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+	
+		var x = 1;
+		var y = 1;
+		var t = x + "," + y + ",30";
+		alert("dedede")
+        $('.middle-east').attr('coords', x + "," + y + ",30");
+	
+});
---
+++ script.js
@@ -0,0 +1,32 @@
+$(document).ready(function() {
+    $('.west-australia').click(function() {
+        $('.currently-selected-country').text('West Australia');
+    });
+	$('.east-australia').click(function() {
+        $('.currently-selected-country').text('East Australia');
+    });
+	$('.new-guinea').click(function() {
+        $('.currently-selected-country').text('New Guinea');
+    });
+	$('.indonesia').click(function() {
+        $('.currently-selected-country').text('Indonesia');
+	});
+	$('.sian').click(function() {
+        $('.currently-selected-country').text('Sian');
+	});
+	$('.india').click(function() {
+        $('.currently-selected-country').text('India');
+	});
+	$('.china').click(function() {
+        $('.currently-selected-country').text('China');
+	});
+	$('.mongolia').click(function() {
+        $('.currently-selected-country').text('Mongolia');
+	});
+	$('.japan').click(function() {
+        $('.currently-selected-country').text('Japan');
+	});
+	$('.middle-east').click(function() {
+        $('.currently-selected-country').text('Middle East');
+	});
+});
